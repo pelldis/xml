d386a3b757703ddb1c494268cb34ae3193d12c1f
pelldis <44728514+pelldis@users.noreply.github.com>
2018-11-10 22:08:55 +0300

    Update xml_parser.py

--- xml_parser.py
+++ xml_parser.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 import sys
 import os
 
-
+# функция для сбора значений из двух рядом стоящих ячеек в список []
 def get_data_xml(filename):
 	tree = ET.parse(filename)
 	root = tree.getroot()
@@ -12,7 +12,6 @@ def get_data_xml(filename):
 	data = []
 	n = 0 # счетчик строк
 	for elem in iter_: 
-		#print n, elem.tag
 		# проходим по файлам откуда забирать значения
 		if "Row" in elem.tag:
 			z = 0 # счетчик столбцов
@@ -20,19 +19,21 @@ def get_data_xml(filename):
 
 		elif "Data" in elem.tag and n > 6:
 			if z > 1 and z < 4: # Берем данные из 3 и 4 столбца
-				print elem.text,
-				data.append(int(elem.text)) # Добавляем значение в список
-				z += 1
+				# Добавляем значение в список
+				data.append(int(elem.text)) 
+				z += 1						
 			else:
 				z += 1
 	#print data   #, data.pop(0)
 	return data
-#get_data_xml("one1.xml")
-
+# функция для обхода файлов xml в указанной директории с вложенной 
+# функцией сбора значений, изменение значений файла filename
+# в двух столбцах для каждого файла и запись в файл filename
 def editXML(directory, filename):
 	files = os.listdir(directory)
 	num = 2 # счетчик для перехода через 2 столбца каждый новый файл
-	for file_ in sorted(files): # парсим каждый файл в сортированном виде
+	# парсим каждый файл в сортированном виде
+	for file_ in sorted(files): 
 		# если файл - xml и не основной файл
 		if file_.endswith(".xml") and "vyg" not in file_:
 			print
@@ -46,28 +47,32 @@ def editXML(directory, filename):
 			n = 0 # счетчик строк
 			c = 0 # счетчик для элемента в листе дочернего файла
 			for elem in iter_: 
-				#print n, elem.tag
-				# проходим по файлам откуда забирать значения
+				# итерация файла filename
+				# Row - тэг строки в дереве
 				if "Row" in elem.tag:
 					z = 0 # счетчик столбцов
-					n += 1
-
+					n += 1 #
+				# проверяем значения с 7 строки для редактирования
 				elif "Data" in elem.tag and n > 6:
-					if z in range(num, num+2): # Берем данные из 2 и 3 столбца
-						print elem.text, int(a[c])
+					# Берем данные из 2 и 3 столбца
+					if z in range(num, num+2): 
+						#print elem.text, int(a[c])
+						# Обязательно преобразуем число в строку
 						elem.text = str(a[c])
 						c += 1
 						z += 1
 					else:
 						z += 1
 			num += 2
-			#vyvod = tree.getroot()
-			#iter_ = tree.getiterator()
-			for elem in iter_:
-				if "Data" in elem.tag:
-					print elem.text
-			tree.write("vygruzka.xml")
-			
-			
+			# пишем в файл после обхода каждого файла
+			tree.write("vygruzka.xml", encoding="UTF-8")
+	# выводим в терминал то, что записали в файл
+	for elem in iter_:
+		if "Row" in elem.tag:
+			print
+		elif "Data" in elem.tag:
+			print "{:<14}".format(elem.text),
+					
 directory = "/home/antosh/Documents"
+# вызов функции
 editXML(directory, "vygruzka.xml")
